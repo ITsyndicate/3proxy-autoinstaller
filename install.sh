48195cb45ebe4d08bb246136a6c756e1fc3cae3e
Alex Kondratiev <xelak.kondratev@gmail.com>
2014-04-23 18:09:30 +0300

    Create install.sh
    install.sh script added

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Attention! You are running this script on your risk. Internet connection could be lost.
+#This scipt will run in background. If you do not want to run it in backgrounf run ./3proxyinstall.sh instead.
+./3proxyinstall.sh &
